068c392ec3de907af5f99510e5829ffa6a2b1f96
Jervin Macalawa <jervinmac@yahoo.com>
2023-05-08 13:45:54 +0800

    done

--- lib/config/global.dart
+++ lib/config/global.dart
@@ -3,7 +3,7 @@ class Global{
   // static var url = 'http://3.135.232.117:5000//api/v1';
   // static var url = 'http://3.144.76.35:5000/api/v1';
   //  static var url = 'https://1568-112-198-104-153.ngrok.io/api/v1';
-    // static var url = 'http://3.34.188.21:8000/api/v1';
-static var url = 'http://10.0.2.2:8000/api/v1';
+    static var url = 'http://3.112.82.82:8000/api/v1';
+// static var url = 'http://10.0.2.2:8000/api/v1';
   static var email = '';
 }
--- lib/online-exercise/online-exercise.dart
+++ lib/online-exercise/online-exercise.dart
@@ -24,7 +24,7 @@ class _OnlineExerciseState extends State<OnlineExercise> {
   late List<VideoPlayerController> _controller = [];
   final List<Map<String, dynamic>> items = [
     {
-      'title': '1. Lunges',
+      'title': '1. Squat',
       'description':
           'To perform lunges, start by standing with your feet hip-width apart and take a step forward with your right foot. Lower your left knee to the ground, keeping your right knee at a 90-degree angle and your right thigh parallel to the ground. Push through your right heel to return to the starting position, and repeat with your left foot. Keep your knees aligned with your ankles, your core engaged, and your back straight throughout the exercise. Perform 10 to 12 repetitions on each side, and consult with your healthcare provider before starting any new exercise program.',
       'image': 'assets/easy/lunges.gif',
@@ -32,7 +32,7 @@ class _OnlineExerciseState extends State<OnlineExercise> {
           'https://chatmind.s3.ap-northeast-1.amazonaws.com/Videos/telegram-cloud-document-5-6233420623779991311.mp4'
     },
     {
-      'title': '2. Pushup',
+      'title': '2. Side crunch',
       'description':
           'To perform a pushup, start in a plank position with your hands shoulder-width apart, fingers facing forward, and your feet together. Lower your body toward the ground by bending your elbows, keeping your core engaged and your back straight. Lower your chest until it almost touches the ground, and then push back up until your arms are fully extended. Keep your elbows close to your body throughout the exercise, and avoid letting them flare out to the sides. Repeat for 10 to 12 repetitions, or as many as you can do with good form. To modify the exercise, you can perform pushups on your knees or against a wall. Always consult with your healthcare provider before starting any new exercise program.',
       'image': 'assets/easy/pushup.gif',
@@ -48,7 +48,7 @@ class _OnlineExerciseState extends State<OnlineExercise> {
           'https://chatmind.s3.ap-northeast-1.amazonaws.com/Videos/telegram-cloud-document-5-6233420623779991323.mp4'
     },
     {
-      'title': '4. Squat',
+      'title': '4. Pulling Exercise',
       'description':
           'To perform a squat, stand with your feet hip-width apart and your toes pointing forward or slightly outward. Lower your body by bending your knees and pushing your hips back, keeping your back straight and your core engaged. Lower your body until your thighs are parallel to the ground, and then push through your heels to stand back up to the starting position. Keep your knees aligned with your toes and avoid letting them collapse inward. You can perform squats with bodyweight or with added weight like a barbell or dumbbells. Repeat for 10 to 12 repetitions, or as many as you can do with good form. Always consult with your healthcare provider before starting any new exercise program.',
       'image': 'assets/easy/squat.gif',
@@ -56,7 +56,7 @@ class _OnlineExerciseState extends State<OnlineExercise> {
           'https://chatmind.s3.ap-northeast-1.amazonaws.com/Videos/telegram-cloud-document-5-6233420623779991322.mp4'
     },
     {
-      'title': '4. Dumbbell Bicep Curl',
+      'title': '5. Dumbbell Bicep Curl',
       'description':
           'The Dumbbell Bicep Curl is a classic exercise that targets the biceps, one of the primary muscles in your upper arm. Its a great way to build arm strength, increase muscle tone, and improve your overall fitness level.',
       'image': 'assets/easy/dumbell-1.gif',
@@ -71,7 +71,7 @@ class _OnlineExerciseState extends State<OnlineExercise> {
 
    final List<Map<String, dynamic>> items2 = [
     {
-      'title': '1. Lunges',
+      'title': '1. Pull Chest Exercise',
       'description':
           'To perform lunges, start by standing with your feet hip-width apart and take a step forward with your right foot. Lower your left knee to the ground, keeping your right knee at a 90-degree angle and your right thigh parallel to the ground. Push through your right heel to return to the starting position, and repeat with your left foot. Keep your knees aligned with your ankles, your core engaged, and your back straight throughout the exercise. Perform 10 to 12 repetitions on each side, and consult with your healthcare provider before starting any new exercise program.',
       'image': 'assets/easy/lunges.gif',
@@ -79,7 +79,7 @@ class _OnlineExerciseState extends State<OnlineExercise> {
           'https://chatmind.s3.ap-northeast-1.amazonaws.com/Videos/telegram-cloud-document-5-6233420623779991320.mp4'
     },
     {
-      'title': '2. Pushup',
+      'title': '2. Dumbbell Reverse Fly',
       'description':
           'To perform a pushup, start in a plank position with your hands shoulder-width apart, fingers facing forward, and your feet together. Lower your body toward the ground by bending your elbows, keeping your core engaged and your back straight. Lower your chest until it almost touches the ground, and then push back up until your arms are fully extended. Keep your elbows close to your body throughout the exercise, and avoid letting them flare out to the sides. Repeat for 10 to 12 repetitions, or as many as you can do with good form. To modify the exercise, you can perform pushups on your knees or against a wall. Always consult with your healthcare provider before starting any new exercise program.',
       'image': 'assets/easy/pushup.gif',
@@ -95,7 +95,7 @@ class _OnlineExerciseState extends State<OnlineExercise> {
           'https://chatmind.s3.ap-northeast-1.amazonaws.com/Videos/telegram-cloud-document-5-6233420623779991318.mp4'
     },
     {
-      'title': '4. Squat',
+      'title': '4. Back Exercise',
       'description':
           'To perform a squat, stand with your feet hip-width apart and your toes pointing forward or slightly outward. Lower your body by bending your knees and pushing your hips back, keeping your back straight and your core engaged. Lower your body until your thighs are parallel to the ground, and then push through your heels to stand back up to the starting position. Keep your knees aligned with your toes and avoid letting them collapse inward. You can perform squats with bodyweight or with added weight like a barbell or dumbbells. Repeat for 10 to 12 repetitions, or as many as you can do with good form. Always consult with your healthcare provider before starting any new exercise program.',
       'image': 'assets/easy/squat.gif',
@@ -103,7 +103,7 @@ class _OnlineExerciseState extends State<OnlineExercise> {
           'https://chatmind.s3.ap-northeast-1.amazonaws.com/Videos/telegram-cloud-document-5-6233420623779991317.mp4'
     },
     {
-      'title': '4. Dumbbell Bicep Curl',
+      'title': '5. Jumping',
       'description':
           'The Dumbbell Bicep Curl is a classic exercise that targets the biceps, one of the primary muscles in your upper arm. Its a great way to build arm strength, increase muscle tone, and improve your overall fitness level.',
       'image': 'assets/easy/dumbell-1.gif',
